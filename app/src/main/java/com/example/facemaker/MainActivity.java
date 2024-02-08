ff4444242f5896f5c5868ae95c180d8c9f33ec6a
joshu <joshuakrasn@gmail.com>
2024-02-08 12:31:07 -0800

    commit 1

--- app/src/main/java/com/example/facemaker/MainActivity.java
+++ app/src/main/java/com/example/facemaker/MainActivity.java
@@ -1,3 +1,8 @@
+//////////////////////////////////
+// @author Joshua Krasnogorov
+// @version 2/8/2024
+//////////////////////////////////
+
 package com.example.facemaker;
 
 import androidx.appcompat.app.AppCompatActivity;
